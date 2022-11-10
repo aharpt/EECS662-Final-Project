90fc66faef0a3a8edb889be114a3fffd610fcc38
Alice Kuang <yuchun.kuang@gmail.com>
2022-11-10 16:24:21 -0600

    Update Termlang list, add typelang

--- fp.hs
+++ fp.hs
@@ -6,12 +6,33 @@
 import Control.Monad
 
 -- AST and Type Definitions
-data TERMLANG = Boolean Bool
+data TYPELANG = TNum
+              | TBool
+                deriving (Show,Eq)
+
+data TERMLANG = Num  Int 
+	          | Boolean Bool -- True False
+              | Id String 
+              | Plus TERMLANG TERMLANG 
+              | Minus TERMLANG TERMLANG
+              | Mult TERMLANG TERMLANG
+              | Div TERMLANG TERMLANG
+	          | Lambda String TERMLANG TERMLANG-- may need to be udpated
+	          | App TERMLANG TERMLANG TERMLANG
+              | Bind String TERMLANG TERMLANG
+              | If TERMLANG TERMLANG TERMLANG
               | And TERMLANG TERMLANG
               | Or TERMLANG TERMLANG
               | Leq TERMLANG TERMLANG
               | IsZero TERMLANG 
-              | If TERMLANG TERMLANG TERMLANG
-              | Bind String TERMLANG TERMLANG
-              | Id String 
-                deriving (Show,Eq)
+               deriving (Show,Eq)
+
+
+--type Env = [(String,TERMLANG)]
+--type Cont = [(String,TYPELANG)]
+
+-- Exercise 1: Implementing type ---
+typeof :: TERMLANG -> (Maybe TYPELANG)
+typeof _ = Nothing
+
+-- Exercise 2: 
