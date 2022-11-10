73b8519d6b6677d9c3c5acfd130a21989dabf00a
Alice Kuang <yuchun.kuang@gmail.com>
2022-11-10 16:34:26 -0600

    Add typeof methods from previous lab

--- fp.hs
+++ fp.hs
@@ -17,7 +17,7 @@ data TERMLANG = Num  Int
               | Minus TERMLANG TERMLANG
               | Mult TERMLANG TERMLANG
               | Div TERMLANG TERMLANG
-	          | Lambda String TERMLANG TERMLANG-- may need to be udpated
+	          | Lambda String TERMLANG TERMLANG
 	          | App TERMLANG TERMLANG TERMLANG
               | Bind String TERMLANG TERMLANG
               | If TERMLANG TERMLANG TERMLANG
@@ -34,5 +34,40 @@ data TERMLANG = Num  Int
 -- Exercise 1: Implementing type ---
 typeof :: TERMLANG -> (Maybe TYPELANG)
 typeof _ = Nothing
+typeof (Num n) = if n<0 
+					then Nothing 
+					else return TNum
+typeof (Plus l r) = do {TNum <- typeof l;
+						TNum <- typeof r;
+						return TNum}
+typeof (Minus l r) = do {TNum <- typeof l;
+						 TNum <- typeof r;
+						 return TNum}						 
+typeof (Mult l r) = do {TNum <- typeof l;
+						TNum <- typeof r;
+						return TNum}
+typeof (Div l r) = do {TNum <- typeof l;
+					   TNum <- typeof r;
+					   return TNum}
+typeof (Boolean b) = return TBool
+typeof (And l r) = do {TBool <- typeof l;
+					   TBool <- typeof r;
+					   return TBool}						 
+typeof (Or l r) = do {TBool <- typeof l;
+					  TBool <- typeof r;
+					  return TBool}
+typeof (IsZero x) = do {TNum <- typeof x;
+						return TBool	}
+typeof (Leq l r) = do {TNum <- typeof l;
+					   TNum <- typeof r;
+					   return TBool}		
+typeof (If c t e) = do {TBool <- typeof c;
+                        t' <- typeof t;
+                        e' <- typeof e;
+                        if t'==e' then return t' else Nothing}
+typeof (Bind i v b) = do {tv <- typeof v;
+					      typeofM ((i,tv):g) b}
+typeof (Id i) = (lookup i g)
+
 
 -- Exercise 2: 
